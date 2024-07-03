6c91171ee142cfa0671901bbea105217ee02d40c
Eugen <vasilenkoevgen88@gmail.com>
2024-07-03 19:25:40 +0300

    refactor: main to styled

--- src/common/components/layout/main/main.tsx
+++ src/common/components/layout/main/main.tsx
@@ -1,17 +1,23 @@
 import {UserSVG} from "../../../../assets/icons/user";
+import styled from "styled-components";
 
 export const Main = () => {
     return (
         <main>
             <div>
-                <span>Hi !</span>
-                <h2>I’m Muhamad Raul.</h2>
-                <h1>a Front-End Developer</h1>
-                <p>Front-End devel oper based in Bekasi, Indonesian
+                <StyledSpan>Hi !</StyledSpan>
+                <StyledH2>I’m Muhamad Raul.</StyledH2>
+                <StyledH1>a Front-End Developer</StyledH1>
+                <StyledP>Front-End devel oper based in Bekasi, Indonesian
                     I’am coding with a clean and beautiful problem
-                    solving in mind.</p>
+                    solving in mind.</StyledP>
             </div>
             <UserSVG/>
         </main>
     );
 };
+
+const StyledSpan= styled.span``
+const StyledH1= styled.h1``
+const StyledH2= styled.h2``
+const StyledP= styled.p``
