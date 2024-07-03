28452f2b5146b49158c4e6762736409a3ddbb5cc
Eugen <vasilenkoevgen88@gmail.com>
2024-07-03 19:20:04 +0300

    second

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css';
+import styled from 'styled-components';
+import {Main} from "./common/components/layout/main/main";
+
+
+function App() {
+    return (
+        <div className="App">
+            <Main/>
+        </div>
+    );
+}
+
+export default App;
+
+const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: #e91e63;
+`;
