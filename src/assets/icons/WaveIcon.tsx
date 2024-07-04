5871c57bf387cf6e4ad5327ac9b13e7a7e087013
Eugen <vasilenkoevgen88@gmail.com>
2024-07-04 22:26:03 +0300

    feat: wave full wigth

--- src/assets/icons/WaveIcon.tsx
+++ src/assets/icons/WaveIcon.tsx
@@ -3,16 +3,18 @@ import { SVGProps, memo } from "react"
 const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
-        width={1440}
-        height={330}
+        viewBox="0 0 1440 330"
+        width="100%" // Устанавливаем ширину в процентах
+        height="100%" // Устанавливаем высоту в процентах
         fill="none"
-        preserveAspectRatio={"none"}
+        preserveAspectRatio="none"
 
         {...props}
     >
         <path
             fill="#4581F6"
-            preserveAspectRatio={"none"}
+            width="100%" // Устанавливаем ширину в процентах
+            height="100%"
             d="m-201 330 57.3-41.2c57.3-41.2 171.9-123.6 286.5-123.6s229.2 82.4 343.8 82.4c114.6 0 229.2-82.4 343.8-96.048 114.6-14.677 229.2 41.973 343.8 27.295C1288.8 165.2 1403.4 82.8 1460.7 41.6L1518 .4V330H-201Z"
         />
     </svg>
--- src/common/components/layout/sections/footer/Footer.tsx
+++ src/common/components/layout/sections/footer/Footer.tsx
@@ -1,49 +1,59 @@
-import {TitleSection} from "../../../ui/titleSections/TitleSection";
-import {FlexWrapper} from "../../../ui/flexWrapper/FlexWrapper";
-import {GithubIcon} from "../../../../../assets/icons/githubIcon";
-import {InstagramIcon} from "../../../../../assets/icons/instagramIcon";
-import {EmailIcon} from "../../../../../assets/icons/emailIcon";
-import {PhoneIcon} from "../../../../../assets/icons/phoneIcon";
-import {WaveIcon} from "../../../../../assets/icons/WaveIcon";
+import { TitleSection } from "../../../ui/titleSections/TitleSection";
+import { FlexWrapper } from "../../../ui/flexWrapper/FlexWrapper";
+import { GithubIcon } from "../../../../../assets/icons/githubIcon";
+import { InstagramIcon } from "../../../../../assets/icons/instagramIcon";
+import { EmailIcon } from "../../../../../assets/icons/emailIcon";
+import { PhoneIcon } from "../../../../../assets/icons/phoneIcon";
+import { WaveIcon } from "../../../../../assets/icons/WaveIcon";
 import styled from "styled-components";
 
 export const Footer = () => {
     return (
         <FlexWrapper id={"footer"} flexDirection={"column"}>
-            <TitleSection title={"Contact Me"}/>
-            <StyledDiv >
+            <TitleSection title={"Contact Me"} />
+            <StyledDiv>
                 <FlexWrapper flexDirection={"column"}>
-                    <GithubIcon/>
-                    <TitleSection title={"Github"}/>
+                    <GithubIcon />
+                    <TitleSection title={"Github"} />
                 </FlexWrapper>
                 <FlexWrapper flexDirection={"column"}>
-                    <InstagramIcon/>
-                    <TitleSection title={"Github"}/>
+                    <InstagramIcon />
+                    <TitleSection title={"Instagram"} />
                 </FlexWrapper>
                 <FlexWrapper flexDirection={"column"}>
-                    <EmailIcon/>
-                    <TitleSection title={"Github"}/>
+                    <EmailIcon />
+                    <TitleSection title={"Email"} />
                 </FlexWrapper>
                 <FlexWrapper flexDirection={"column"}>
-                    <PhoneIcon/>
-                    <TitleSection title={"Github"}/>
+                    <PhoneIcon />
+                    <TitleSection title={"Phone"} />
                 </FlexWrapper>
-            </StyledDiv >
-            <WaveIcon style={{position: "absolute", bottom: "0", width:"100%", height:"350px", left:"0px", right:"0px", backgroundRepeat: "no-repeat", backgroundSize: "100%"}}/>
+                <WaveBackground>
+                    <WaveIcon />
+                </WaveBackground>
+            </StyledDiv>
         </FlexWrapper>
     );
 };
 
 const StyledDiv = styled.div`
+    position: relative;
+    width: 100%;
+    //height: 100vh;
     display: flex;
-flex-direction: row;
-    justify-content: center;
-    //position: relative;
-    //:before{
-    //    position: absolute;
-    //    width: 100%;
-    //    height: 100px;
-    //    content: '';
-    //    background-image: url("../../../../../assets/icons/WavePng.png");
-    //}
-`
+    justify-content: space-around;
+    flex-direction: row;
+`;
+
+const WaveBackground = styled.div`
+    position: absolute;
+    bottom: -5px;
+    left: 0;
+    width: 100%;
+    z-index: -1;
+
+    svg {
+        width: 100%;
+        //height: 100%;
+    }
+`;
