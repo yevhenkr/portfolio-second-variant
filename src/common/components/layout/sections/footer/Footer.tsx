f0f3d48b76052156236fad8fee3c0f484ae12bee
Eugen <vasilenkoevgen88@gmail.com>
2024-07-10 19:59:05 +0300

    refactor

--- src/common/components/layout/sections/footer/Footer.tsx
+++ src/common/components/layout/sections/footer/Footer.tsx
@@ -5,11 +5,12 @@ import { InstagramIcon } from "../../../../../assets/icons/instagramIcon";
 import { EmailIcon } from "../../../../../assets/icons/emailIcon";
 import { PhoneIcon } from "../../../../../assets/icons/phoneIcon";
 import { WaveIcon } from "../../../../../assets/icons/WaveIcon";
+import  Vector  from "../../../../../assets/img/VectorWave.png";
 import styled from "styled-components";
 
 export const Footer = () => {
     return (
-        <FlexWrapper id={"footer"} flexDirection={"column"} max_width={"1628px"} width={"100%"}>
+        <StyledFooter >
             <TitleSection wordOne={"Contact"} wordSecond={"Me"}/>
             <StyledDiv>
                 <FlexWrapper flexDirection={"column"}>
@@ -28,17 +29,43 @@ export const Footer = () => {
                     <PhoneIcon/>
                     <span>Phone</span>
                 </FlexWrapper>
-                <WaveBackground>
-                <WaveIcon />
-                </WaveBackground>
+                {/*<WaveBackground>*/}
+                {/*<WaveIcon />*/}
+                {/*</WaveBackground>*/}
             </StyledDiv>
-        </FlexWrapper>
+        </StyledFooter>
     );
 };
+
+const StyledFooter = styled.footer`
+    id: "#footer";
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    position: relative;
+
+    &::before {
+        content: "";
+        position: absolute;
+        right: 0;
+        left: 0;
+        bottom: 0;
+        z-index: -1;
+        width: 100%;
+        height: 36vh;
+        max-height: 380px;
+        transform: translateY(50%);
+        background-image: url(${Vector});
+        background-repeat: no-repeat;
+        background-size: 100%;
+    }
+
+`
 
 const StyledDiv = styled.div`
     position: relative;
     width: 100%;
+    margin: 0 136px;
     max-width: 1628px;
     display: flex;
     justify-content: space-around;
